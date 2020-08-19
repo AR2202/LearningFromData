1eddedfeaf87849ad7f43da65b534a936b9adcc3
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-08-19 22:50:13 +0100

    added homework 1

--- src/hw1/PLA.hs
+++ src/hw1/PLA.hs
@@ -2,7 +2,10 @@
 {-#LANGUAGE TypeSynonymInstances, FlexibleInstances, ScopedTypeVariables, TemplateHaskell #-}
 module PLA
 (
-Weight(..)
+Weight(..),
+Point'(..),
+viewX,
+trainPLA
 )
 where
 import Control.Monad.Reader
@@ -10,6 +13,8 @@ import Data.List
 import System.Random
 import Data.Traversable(for)
 import Control.Lens
+import Control.Lens.Operators
+import Control.Lens.Combinators
 
 --------------------------------
 --  Solutions to homework 1, problems 7-10 of
@@ -24,8 +29,10 @@ import Control.Lens
 
 type Weight = (Float,Float,Float)
 type Point = (Float,Float)
+-- | To be used at a later stage - currently just a test
 data Point' = Point'{_x::Float,_y::Float}
 makeLenses ''Point'
+
 instance Num Weight where
     (+) (x0,x1,x2) (w0,w1,w2) = (w0+x0,  w1+x1,  w2+x2)
     (-) (x0,x1,x2)(w0,w1,w2)=(x0-w0,x1-w1,x2-w2)
@@ -57,7 +64,7 @@ dotProd (x0,x1,x2) (w0,w1,w2) = w0*x0 + w1*x1 + w2*x2
 
 -- | Scalar multiplication of the weight vecotr
 prod :: Float -> Weight  -> Weight
-prod yn (x0,x1,x2)  = (x0*yn,x1*yn,x2*yn)
+prod lambda (x0,x1,x2)  = (x0*lambda,x1*lambda,x2*lambda)
 
 -- | Converting a point to a weight by adding the first coordinate (1)
 point2weight :: Point -> Weight
@@ -67,51 +74,33 @@ point2weight (x1,x2) =(1,x1,x2)
 yn :: (Float -> Float) -> Point -> Float
 yn yLine (x1,x2) = if yLine x1 < x2 then (-1) else 1
 
--- | The sign function
-sign (w0,w1,w2) = signum(w0+w1+w2)
 
--- | The CrossEntropy error of one point
-crossEntropy1 yLine w xn = log (1+ exp(-1*y1 *dotProd xw w))
-    where y1 = yn yLine xn
-          xw = point2weight xn
+-- | Checking if a point is misclassified
+isMisclassified yLine weight point = classified /= label
+    where classified = signum $ dotProd (point2weight point) weight
+          label = yn yLine point
 
- -- | The CrossEntropy error of a list of points         
-crossEntropy yLine w points = (*) 0.01 $ sum $ map (crossEntropy1 yLine w) points
-
--- | The gradient of the CrossEntropy with respect to the weight for one point
-dCrossEntropy1 :: (Float->Float) -> Weight -> Point -> Weight
-dCrossEntropy1 yLine w xn = prod (-1/(1+ exp(y1* dotProd xw w)))(prod y1 xw ) 
-    where y1 = yn yLine xn
-          xw = point2weight xn
-
--- | The update function for the weight
-updateWeight yLine  eta w xn =  w - prod  eta (dCrossEntropy1 yLine w xn) 
-
+updateWeight' yLine w xn = w + prod (yn yLine xn)  (point2weight xn)
 -- | Performs 1 training epoch (one run through all training points)
-epoch yLine eta = foldl' (updateWeight yLine eta) 
+
+
+-- | performs the perceptron learning algorithm
+pla yLine w niter gen xs 
+    |misclassifiedXs == [] = (w, niter)
+    |otherwise =  pla yLine wUpdated (niter+1) nextRandom xs
+    where wUpdated = updateWeight' yLine w $ misclassifiedXs!! number
+          misclassifiedXs  = filter (isMisclassified yLine w) xs
+          randomRange = (0,length misclassifiedXs - 1)
+          (number,nextRandom) = randomR randomRange gen
 
 -- | stores the target function and performs training, returning the weight and the number of epochs performed before convergence
-weightReader :: [Point] -> Reader (Float ->Float) (Weight,Int)
-weightReader points = do
+weightReader' :: [Point] -> StdGen-> Reader (Float ->Float) (Weight,Int)
+weightReader' points gen = do
     targetFunc <-ask
-    let pointpermutations = pointPermutations points
-    let weightsEpochs = finalWeights 0.01 targetFunc 0.01 (0,0,0) pointpermutations 0
+    let weightsEpochs = pla targetFunc (0,0,0) 0 gen points
     return weightsEpochs
 
--- | performs testing and returns the CrossEntropy error
-errReader :: [Point] -> Weight-> Reader (Float ->Float) Float
-errReader points weights = do
-    targetFunc <-ask
-    let testError = crossEntropy  targetFunc weights points
-    return testError
 
--- | performs training epochs until the stop criterion for convergence is reached
-finalWeights stopcrit yLine eta weight0 pointpermutations epochs
-    |vecval (weight1 -weight0)<stopcrit = (weight1,epochs)
-    |otherwise = finalWeights stopcrit yLine eta weight1 (tail  pointpermutations) (epochs+1)
-        where 
-            weight1 = epoch yLine eta weight0 $Prelude.head pointpermutations
-            vecval (w0,w1,w2) = sqrt $ w0**2 + w1**2 + w2**2
 
 -- |Creates a random Point between (-1,-1) and (1,1)
 createRandomPoint = do
@@ -128,25 +117,30 @@ createRandomPoints n  = do
  -- | creates an infinite list of permutations of the list provided as an argument  
 pointPermutations = cycle . permutations 
 
+-- | Just a test for the point lens
+viewX  point = view x point
 ---------------------------------------------------------------
 -- The final loop that creates the target function and training and testing data,
 -- performs training and testing
 -------------------------------------------------------------------
 
--- | performs the whole training and testing loop, printing the weights, epochs and the test error
-trainAndTest = do
+-- | performs the PLA
+trainPLA:: Int -> IO()
+trainPLA nPoints = do
     point1 <- createRandomPoint
     point2 <- createRandomPoint
-
+    stdgen <- getStdGen
     let target = line point1 point2
-    trainpoints <- createRandomPoints 100
-    let (weights,epochs) = runReader (weightReader trainpoints)  target
+    trainpoints <- createRandomPoints nPoints
+    let (weights,epochs) = runReader (weightReader' trainpoints stdgen)  target
     putStr "Weights: "
     print weights
     putStr "Epochs: "
     print epochs
-    testpoints <- createRandomPoints 100
-    let testError = runReader (errReader testpoints weights)  target
-    putStr "Eout: "
-    print testError
+    testpoints <- createRandomPoints 1000
+    let misclassifiedTestPoints = length $ filter (isMisclassified target weights) testpoints
+    let prob = fromIntegral misclassifiedTestPoints/1000
+    putStr "Probability f /= g: "
+    print prob
+
 
