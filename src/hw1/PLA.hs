6e6309c47b8b6bf0d24f2c625ff5b7ab511e5b3d
Annika Rings <annika.rings@dpag.ox.ac.uk>
2020-09-08 21:30:41 +0100

    added homework 2 problem 7

--- src/hw1/PLA.hs
+++ src/hw1/PLA.hs
@@ -5,7 +5,8 @@ module PLA
 Weight(..),
 Point'(..),
 viewX,
-trainPLA
+trainPLA,
+trainPLAWithInitial
 )
 where
 import Control.Monad.Reader
@@ -15,6 +16,7 @@ import Data.Traversable(for)
 import Control.Lens
 import Control.Lens.Operators
 import Control.Lens.Combinators
+import Numeric.LinearAlgebra.Data
 
 --------------------------------
 --  Solutions to homework 1, problems 7-10 of
@@ -27,10 +29,10 @@ import Control.Lens.Combinators
 -- Types and Typeclass Instances
 --------------------------------
 
-type Weight = (Float,Float,Float)
-type Point = (Float,Float)
+type Weight = (R,R,R)
+type Point = (R,R)
 -- | To be used at a later stage - currently just a test
-data Point' = Point'{_x::Float,_y::Float}
+data Point' = Point'{_x::R,_y::R}
 makeLenses ''Point'
 
 instance Num Weight where
@@ -44,14 +46,14 @@ instance Num Weight where
 ---------------------------------------------
 -- Creating the line for the target function
 --------------------------------------------
-m :: Point -> Point -> Float
+m :: Point -> Point -> R
 m (x1,x2)(z1,z2) = (x2 - z2)/(x1 - z1)
 
-b :: Point -> Point -> Float
+b :: Point -> Point -> R
 b (x1,x2) (z1,z2) = x2 -x1 *(x2-z2)/(x1-z1)
 
 -- | Takes 2 Points and returns the function of a line connecting them
-line :: Point -> Point -> (Float -> Float)
+line :: Point -> Point -> (R -> R)
 line point1 point2 = \x -> m point1 point2 * x + b point1 point2
 
 -----------------------------------
@@ -59,11 +61,11 @@ line point1 point2 = \x -> m point1 point2 * x + b point1 point2
 ------------------------------------
 
 -- | The dot product
-dotProd :: Weight -> Weight -> Float
+dotProd :: Weight -> Weight -> R
 dotProd (x0,x1,x2) (w0,w1,w2) = w0*x0 + w1*x1 + w2*x2
 
 -- | Scalar multiplication of the weight vecotr
-prod :: Float -> Weight  -> Weight
+prod :: R -> Weight  -> Weight
 prod lambda (x0,x1,x2)  = (x0*lambda,x1*lambda,x2*lambda)
 
 -- | Converting a point to a weight by adding the first coordinate (1)
@@ -71,7 +73,7 @@ point2weight :: Point -> Weight
 point2weight (x1,x2) =(1,x1,x2)
 
 -- | looks up the label of the point by applying the target function
-yn :: (Float -> Float) -> Point -> Float
+yn :: (R -> R) -> Point -> R
 yn yLine (x1,x2) = if yLine x1 < x2 then (-1) else 1
 
 
@@ -94,24 +96,29 @@ pla yLine w niter gen xs
           (number,nextRandom) = randomR randomRange gen
 
 -- | stores the target function and performs training, returning the weight and the number of epochs performed before convergence
-weightReader' :: [Point] -> StdGen-> Reader (Float ->Float) (Weight,Int)
+weightReader' :: [Point] -> StdGen-> Reader (R ->R) (Weight,Int)
 weightReader' points gen = do
     targetFunc <-ask
     let weightsEpochs = pla targetFunc (0,0,0) 0 gen points
     return weightsEpochs
 
-
+-- | stores the target function and performs training, returning the weight and the number of epochs performed before convergence
+weightReader :: [Point] -> StdGen-> Weight->Reader (R ->R) (Weight,Int)
+weightReader points gen initialWeights= do
+    targetFunc <-ask
+    let weightsEpochs = pla targetFunc initialWeights 0 gen points
+    return weightsEpochs
 
 -- |Creates a random Point between (-1,-1) and (1,1)
 createRandomPoint = do
-    rand1 :: Float <- randomRIO (-1,1)
-    rand2 :: Float <- randomRIO (-1,1)
+    rand1 :: R <- randomRIO (-1,1)
+    rand2 :: R <- randomRIO (-1,1)
     return  (rand1,rand2)
 
 -- |Creates a list of n random Points between (-1,-1) and (1,1)
 createRandomPoints n  = do
-    randList1 :: [Float] <- for [1 .. n] $ \_ -> randomRIO (-1,1)
-    randList2 :: [Float] <- for [1 .. n] $ \_ -> randomRIO (-1,1)
+    randList1 :: [R] <- for [1 .. n] $ \_ -> randomRIO (-1,1)
+    randList2 :: [R] <- for [1 .. n] $ \_ -> randomRIO (-1,1)
     return $ zip randList1 randList2
 
  -- | creates an infinite list of permutations of the list provided as an argument  
@@ -142,5 +149,12 @@ trainPLA nPoints = do
     let prob = fromIntegral misclassifiedTestPoints/1000
     putStr "Probability f /= g: "
     print prob
+
+-- | performs the PLA with a set of initial weights, training points and a target function
+trainPLAWithInitial :: (R, R, R) -> [Point] -> (R -> R) -> IO (Weight,Int)
+trainPLAWithInitial initialWeights trainPoints target = do
+    stdgen <- getStdGen
+    let (weights,epochs) = runReader (weightReader trainPoints stdgen initialWeights)  target
+    return(weights,epochs)
 
 
--- src/hw2/LinearRegression.hs
+++ src/hw2/LinearRegression.hs
@@ -2,7 +2,8 @@
 {-#LANGUAGE FlexibleContexts#-}
 module LinearRegression
 (trainLinReg,
-avError
+avError,
+linRegAsInitialForPLA
 
 )
 where
@@ -12,6 +13,8 @@ import Numeric.LinearAlgebra.HMatrix
 import System.Random
 import Data.Traversable (for)
 import Control.Monad(replicateM)
+import PLA
+import Data.Maybe
 
 --------------------------------------------------------------
 -- Solutions to homework 2 of "Learning from data" question 5-6
@@ -20,14 +23,18 @@ import Control.Monad(replicateM)
 ---------------------------------------------
 -- Creating the line for the target function
 --------------------------------------------
+
+-- | takes 2 points and returns the slope of the line connecting them
 m :: (R,R) -> (R,R) -> R
 m (x1,x2)(z1,z2) = (x2 - z2)/(x1 - z1)
 
+-- | takes 2 points and returns the y-intercept of the line connecting them
 b :: (R,R) -> (R,R) -> R
 b (x1,x2) (z1,z2) = x2 -x1 *(x2-z2)/(x1-z1)
 
 -- | Takes 2 Points and returns the function of a line connecting them
 line point1 point2 = \x -> m point1 point2 * x + b point1 point2
+
 -----------------------------------
 -- Creating random data points
 ------------------------------------
@@ -83,23 +90,41 @@ linRegClassificationError matrixOfInputData vectorOfLabels weight = numberMiscla
 ----------------------------------------------
 -- training and testing the Linear Regression model
 
--- | training linear Regression on random dataset of 100 points and testing on 1000 points and returning a pair of in-Sample and out-of-sample error
-trainLinReg :: IO (R,R)
-trainLinReg = do
+
+makeTargetFunction :: IO (R->R)
+makeTargetFunction = do
     point1 <- createRandomPoint
     point2 <- createRandomPoint
     let target = line point1 point2
-    trainpoints <- createRandomPoints 100
-    testpoints <- createRandomPoints 1000
+    return target
+
+-- | training linear Regression on random dataset of 100 points and testing on 1000 points and returning a pair of in-Sample and out-of-sample error
+--trainLinReg :: IO (Matrix R,Vector R,Vector R)
+trainLinReg target n = do    
+    trainpoints <- createRandomPoints n
+    
     let trainX = createMatrixX trainpoints
     let trainY = createVectorY target trainpoints
     let weights = linearRegressionWeight trainX trainY
+    let inSampleError = linRegClassificationError trainX trainY weights
+    return (weights,inSampleError,trainpoints)
+
+testLinReg target n weights= do
+    testpoints <- createRandomPoints n
     let testX = createMatrixX testpoints
     let testY = createVectorY target testpoints
 
-    let inSampleError = linRegClassificationError trainX trainY weights
+    
     let outOfSampleError = linRegClassificationError testX testY weights
-    return (inSampleError, outOfSampleError)
+    return  outOfSampleError
+
+trainAndTestLinReg = do
+    target<-makeTargetFunction
+    (weights,inSampleError,_)<-trainLinReg target 100
+    outOfSampleError<- testLinReg target 1000 weights
+    return (inSampleError,outOfSampleError)
+
+
 ---------------------------------------------------------
 -- averaging several runs of the algorithm
 ---------------------------------------------------------
@@ -116,4 +141,25 @@ averageErrors pairs = (avFst,avSnd)
 
 -- |pair of average in-sample and out-of-sample error in n runs of linear Regression on n different sets of points
 avError :: Int -> IO ()
-avError n = putStrLn .( "In-sample error, out-of-sample error: " ++).show =<< fmap averageErrors (replicateM n trainLinReg)
+avError n = putStrLn .( "In-sample error, out-of-sample error: " ++).show =<< fmap averageErrors (replicateM n trainAndTestLinReg)
+
+----------------------------------------------
+-- Using linear regression to obtain initial weights for perceptron
+--------------------------------------------------
+
+listToTuple :: [a] -> Maybe (a,a,a)
+listToTuple (x:y:z:xs) = Just (x,y,z)
+listToTuple _ = Nothing
+
+linRegAsInitialForPLA :: IO()
+linRegAsInitialForPLA = do
+    target<-makeTargetFunction
+    (initialweightsVec,_,trainPoints)<-trainLinReg target 10
+    let maybeInitialWeights = listToTuple $ toList initialweightsVec
+    let initialWeights = fromMaybe (0,0,0) maybeInitialWeights
+    (finalWeights,epochs)<-trainPLAWithInitial initialWeights trainPoints target
+    putStr "Weights: "
+    print finalWeights
+    putStr "Epochs: "
+    print epochs
+    
